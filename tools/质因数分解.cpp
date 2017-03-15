41055247a498a2153e6fc6c631ce7d1dc4cc12ec
Hydroge <sonic626@163.com>
2017-03-15 20:40:00 +0800

    Create 质因数分解.cpp

---
+++ tools/质因数分解.cpp
@@ -0,0 +1,13 @@
+vector<int> factor(int x) {
+	vector<int> ret;
+	for (int i = 2; i * i <= x; i++) {
+		while (x % i == 0) {
+			ret.push_back(i);
+			x /= i;
+		}
+	}
+	if (x > 1) {
+		ret.push_back(x);
+	}
+	return ret;
+}
